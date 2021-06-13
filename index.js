9ab4712024e5452a01f8a732b2423f1a8faeab3d
mdquayle <misternopants@gmail.com>
2021-06-13 10:52:48 -0400

    commit tree 643c120fe3ed6c76c3d3585404efb2f1d44302a0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(array){
+    const win = array.find(({result}) => result === 'W');
+    if (win){
+        return win.year;
+    }
+    else{
+        return undefined
+    }
+}  
